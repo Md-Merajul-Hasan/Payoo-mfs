5f4880bd5c8c794b689f84f3e000a027319cb270
Jim <merajuljim@gmai.com>
2024-09-19 12:58:58 +0600

    added phone mokeup and login form and eventListener to login button of login form

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+})
